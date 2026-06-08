d40d1c05494657675c1e043a69f2696b89f52852
Frédéric Blanc <frederic.blanc@itanea.fr>
2026-06-08 23:33:53 +0200

    chore: bump version to v1.1.1

--- src/BlogrGdprPlugin.php
+++ src/BlogrGdprPlugin.php
@@ -26,7 +26,7 @@ class BlogrGdprPlugin implements BlogrExtension, FilamentPlugin
 
     public function getVersion(): string
     {
-        return '1.1.0';
+        return '1.1.1';
     }
 
     public function getAuthor(): string
